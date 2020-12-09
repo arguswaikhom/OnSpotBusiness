171d554313b812145fa9076f64460aa5ecd8919b
Waikhom,Bebikananda <Bebikananda.Waikhom@Cerner.com>
2020-12-09 23:22:45 +0530

    Refactoring MainActivity.java

--- app/src/main/java/com/crown/onspotbusiness/page/MainActivity.java
+++ app/src/main/java/com/crown/onspotbusiness/page/MainActivity.java
@@ -10,59 +10,75 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.crown.library.onspotlibrary.controller.OSPreferences;
 import com.crown.library.onspotlibrary.model.business.BusinessOSB;
-import com.crown.library.onspotlibrary.model.business.BusinessV6;
 import com.crown.library.onspotlibrary.model.user.UserOSB;
+import com.crown.library.onspotlibrary.utils.OSListUtils;
+import com.crown.library.onspotlibrary.utils.OSString;
 import com.crown.library.onspotlibrary.utils.emun.OSPreferenceKey;
 import com.crown.onspotbusiness.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.crown.onspotbusiness.controller.AppController;
+import com.crown.onspotbusiness.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final String TAG = MainActivity.class.getName();
-    private BottomNavigationView mBottomNavigationView;
+    private UserOSB user;
+    private BusinessOSB business;
     private ListenerRegistration mUserChangeListener;
     private ListenerRegistration mBusinessChangeListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        mBottomNavigationView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(mBottomNavigationView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        if (!AppController.getInstance().isAuthenticated()) {
+            AppController.getInstance().signOut(this);
+            return;
+        }
+
+        OSPreferences preferences = OSPreferences.getInstance(getApplicationContext());
+        user = preferences.getObject(OSPreferenceKey.USER, UserOSB.class);
+        business = preferences.getObject(OSPreferenceKey.BUSINESS, BusinessOSB.class);
+
         syncAccountInfo();
+        verifyDeviceToken();
     }
 
     private void syncAccountInfo() {
-        UserOSB user = OSPreferences.getInstance(getApplicationContext()).getObject(OSPreferenceKey.USER, UserOSB.class);
-        mUserChangeListener = FirebaseFirestore.getInstance().collection(getString(R.string.ref_user)).document(user.getUserId()).addSnapshotListener((documentSnapshot, e) -> {
-            if (documentSnapshot != null && documentSnapshot.exists()) {
-                UserOSB updatedUser = documentSnapshot.toObject(UserOSB.class);
+        FirebaseFirestore instance = FirebaseFirestore.getInstance();
+        mUserChangeListener = instance.collection(OSString.refUser).document(user.getUserId()).addSnapshotListener((doc, e) -> {
+            if (doc != null && doc.exists()) {
+                UserOSB updatedUser = doc.toObject(UserOSB.class);
                 if (updatedUser != null) {
+                    this.user = updatedUser;
                     OSPreferences.getInstance(getApplicationContext()).setObject(updatedUser, OSPreferenceKey.USER);
                     sendBroadcast(new Intent(getString(R.string.action_osb_changes)));
                 }
             }
         });
-        mBusinessChangeListener = FirebaseFirestore.getInstance().collection(getString(R.string.ref_business)).document(user.getBusinessRefId()).addSnapshotListener(((documentSnapshot, e) -> {
-            if (documentSnapshot != null && documentSnapshot.exists()) {
-                BusinessOSB osb = documentSnapshot.toObject(BusinessOSB.class);
+        mBusinessChangeListener = instance.collection(OSString.refBusiness).document(user.getBusinessRefId()).addSnapshotListener(((doc, e) -> {
+            if (doc != null && doc.exists()) {
+                BusinessOSB osb = doc.toObject(BusinessOSB.class);
                 if (osb != null) {
+
                     OSPreferences.getInstance(getApplicationContext()).setObject(osb, OSPreferenceKey.BUSINESS);
                     sendBroadcast(new Intent(getString(R.string.action_osb_business_changes)));
                 }
             }
         }));
+    }
 
-        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(result -> {
-            BusinessV6 business = OSPreferences.getInstance(getApplicationContext()).getObject(OSPreferenceKey.BUSINESS, BusinessV6.class);
-            if (business == null || business.getDeviceToken() == null || business.getDeviceToken().isEmpty() || !business.getDeviceToken().contains(result.getToken())) {
-                FirebaseFirestore.getInstance().collection(getString(R.string.ref_business)).document(user.getBusinessRefId()).update(getString(R.string.field_device_token), FieldValue.arrayUnion(result.getToken()));
+    private void verifyDeviceToken() {
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(token -> {
+            if (business == null || OSListUtils.isEmpty(business.getDeviceToken()) || !business.getDeviceToken().contains(token)) {
+                FirebaseFirestore.getInstance().collection(OSString.refBusiness).document(user.getBusinessRefId())
+                        .update(OSString.fieldDeviceToken, FieldValue.arrayUnion(token));
             }
         });
     }
@@ -70,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mUserChangeListener.remove();
-        mBusinessChangeListener.remove();
+        if (mUserChangeListener != null) mUserChangeListener.remove();
+        if (mBusinessChangeListener != null) mBusinessChangeListener.remove();
     }
 }
